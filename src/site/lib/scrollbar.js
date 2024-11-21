3336030c354c71fcac00ff48254df71da7e919e3
christ-resurrected <dev@me2christ.com>
2024-11-21 14:39:10 +0000

    fix ipad invisible scrollbar

---
+++ src/site/lib/scrollbar.js
@@ -0,0 +1,26 @@
+// fix ipad safari invisible scrollbar
+addEventListener("DOMContentLoaded", () => {
+  const B = document.body
+  const D = document.documentElement
+  const W = window
+
+  var height_total
+
+  function refresh_scrollbar_bg() {
+    var scroll_pos = Math.round(D.scrollTop || B.scrollTop)
+    var pos = scroll_pos / height_total
+    B.style.backgroundColor = (pos < 0.5) ? '#000' : '#fff'
+  }
+
+  function refresh_height() {
+    height_total = (D.scrollHeight || B.scrollHeight) - W.innerHeight
+  }
+
+  // event handlers
+  window.addEventListener('orientationchange', refresh_height)
+  window.addEventListener('resize', refresh_height)
+  window.addEventListener('scroll', refresh_scrollbar_bg)
+
+  // init
+  refresh_height()
+})
