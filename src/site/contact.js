b76aa85fcdfc3cf98101da0f2a856fdad31fb561
christ-resurrected <dev@me2christ.com>
2024-12-18 02:28:30 +0000

    refactor

--- src/site/contact.js
+++ src/site/contact.js
@@ -2,19 +2,19 @@ addEventListener("DOMContentLoaded", () => {
   const elError = document.getElementById('error')
   const elFieldset = document.getElementsByTagName('fieldset')[0]
 
-  document.forms[0].addEventListener('submit', (event) => {
-    event.preventDefault()
+  document.forms[0].addEventListener('submit', (e) => {
+    e.preventDefault()
     elError.style.display = 'none'
     elFieldset.disabled = true
-    req = { method: 'POST', body: new URLSearchParams(new FormData(event.target)) }
-    fetch(event.target.action, req).then(handleResponse).then(handleResponseText).then(showSuccess).catch(showError);
+    req = { method: 'POST', body: new URLSearchParams(new FormData(e.target)) }
+    fetch(e.target.action, req).then(handleResponse).then(handleResponseText).then(showSuccess).catch(showError);
   });
 
   function handleResponse(res) { return res.text() }
   function handleResponseText(t) { if (t != 'OK') throw new Error(t) }
 
-  function showError(e) {
-    elError.innerHTML = e
+  function showError(err) {
+    elError.innerHTML = err
     elError.style.display = 'block'
     elFieldset.disabled = false
   }
