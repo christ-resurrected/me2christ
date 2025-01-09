c326266101c7add1a45f913f8293f428cd4c55d8
christ-resurrected <dev@me2christ.com>
2025-01-09 08:58:59 +0000

    add scrollbar.js to resources.pug

--- src/site/lib/scrollbar.js
+++ src/site/lib/scrollbar.js
@@ -1,30 +1,32 @@
-// ensure iOS overlay scrollbar stays visible when bg color goes from black to white
-addEventListener("DOMContentLoaded", () => {
-  const UA = navigator.userAgent
-  var isIOS = UA.includes('iPad') || UA.includes('iPhone') || UA.includes('iPod')
-  if (!isIOS) return
+// ensure iOS overlay scrollbar stays visible when bg color inverts
+function initScrollbar(bgTop, bgBottom) {
+  addEventListener("DOMContentLoaded", () => {
+    const UA = navigator.userAgent
+    var isIOS = UA.includes('iPad') || UA.includes('iPhone') || UA.includes('iPod')
+    if (!isIOS) return
 
-  const B = document.body
-  const D = document.documentElement
-  const W = window
+    const B = document.body
+    const D = document.documentElement
+    const W = window
 
-  var height_total
+    var height_total
 
-  function refresh_scrollbar_bg() {
-    var scroll_pos = Math.round(D.scrollTop || B.scrollTop)
-    var pos = scroll_pos / height_total
-    B.style.backgroundColor = (pos < 0.5) ? '#000' : '#fff'
-  }
+    function refresh_scrollbar_bg() {
+      var scroll_pos = Math.round(D.scrollTop || B.scrollTop)
+      var pos = scroll_pos / height_total
+      B.style.backgroundColor = (pos < 0.5) ? bgTop : bgBottom
+    }
 
-  function refresh_height() {
-    height_total = (D.scrollHeight || B.scrollHeight) - W.innerHeight
-  }
+    function refresh_height() {
+      height_total = (D.scrollHeight || B.scrollHeight) - W.innerHeight
+    }
 
-  // event handlers
-  window.addEventListener('orientationchange', refresh_height)
-  window.addEventListener('resize', refresh_height)
-  window.addEventListener('scroll', refresh_scrollbar_bg)
+    // event handlers
+    window.addEventListener('orientationchange', refresh_height)
+    window.addEventListener('resize', refresh_height)
+    window.addEventListener('scroll', refresh_scrollbar_bg)
 
-  // init
-  refresh_height()
-})
+    // init
+    refresh_height()
+  })
+}
