34375faedb1c6c49431ca9b497129d7da90c8e47
christ-resurrected <dev@me2christ.com>
2025-02-12 20:11:42 +0000

    add exception handler

--- src/site/ministry.contact.js
+++ src/site/ministry.contact.js
@@ -4,14 +4,19 @@ addEventListener("DOMContentLoaded", () => {
   document.forms[0].addEventListener('submit', (e) => {
     e.preventDefault()
     el('#error').innerHTML = ''
-    req = { method: 'POST', body: new FormData(e.target) }
+    req = { method: 'POST', body: new FormData(e.target), signal: AbortSignal.timeout(10000) }
     disableUI(true) // must happen AFTER reading form!
-    fetch(e.target.action, req).then(handleResponse)
+    fetch(e.target.action, req).then(handleResponse).catch(handleError)
   });
 
   function disableUI(disabled) {
     el('fieldset').disabled = disabled
     el('button').classList.toggle('spinner', disabled)
+  }
+
+  function handleError(res) {
+    disableUI(false)
+    el('#error').innerHTML = res
   }
 
   function handleResponse(res) {
