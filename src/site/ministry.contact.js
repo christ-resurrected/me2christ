0dcaef1bff847e935da459ab2f13f05956dbd00e
christ-resurrected <dev@me2christ.com>
2025-02-12 20:38:25 +0000

    refactor

--- src/site/ministry.contact.js
+++ src/site/ministry.contact.js
@@ -1,26 +1,25 @@
 addEventListener("DOMContentLoaded", () => {
   function el(sel) { return document.querySelector(sel) }
 
-  document.forms[0].addEventListener('submit', (e) => {
-    e.preventDefault()
-    el('#error').innerHTML = ''
-    req = { method: 'POST', body: new FormData(e.target), signal: AbortSignal.timeout(10000) }
-    disableUI(true) // must happen AFTER reading form!
-    fetch(e.target.action, req).then(handleResponse).catch(handleError)
-  });
-
   function disableUI(disabled) {
     el('fieldset').disabled = disabled
     el('button').classList.toggle('spinner', disabled)
   }
 
-  function handleError(res) {
-    disableUI(false)
-    el('#error').innerHTML = res
-  }
-
-  function handleResponse(res) {
-    disableUI(false)
-    res.text().then((text) => (res.status == 200 ? el('#success') : el('#error')).innerHTML = text)
-  }
+  document.forms[0].addEventListener('submit', async (e) => {
+    try {
+      e.preventDefault()
+      el('#error').innerHTML = ''
+      req = { method: 'POST', body: new FormData(e.target), signal: AbortSignal.timeout(10000) }
+      disableUI(true) // must happen AFTER reading form!
+      res = await fetch(e.target.action, req)
+      disableUI(false)
+      text = await res.text();
+      (res.status == 200 ? el('#success') : el('#error')).innerHTML = text
+    }
+    catch (err) {
+      disableUI(false)
+      el('#error').innerHTML = err
+    }
+  });
 })
