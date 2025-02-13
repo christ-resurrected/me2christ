c5ddd13d78af064a3dc2e0178d1f29e47c65f75d
christ-resurrected <dev@me2christ.com>
2025-02-13 14:12:05 +0000

    reload on node restart

--- src/site/lib/livereload.js
+++ src/site/lib/livereload.js
@@ -1,20 +1,26 @@
+var isNodeRestarting = false
 var ws
 
 function connect() {
   ws = new WebSocket(`ws://${location.host.split(':')[0] || 'localhost'}:7778`)
 
+  ws.onclose = function() {
+    console.log('close')
+    isNodeRestarting = true // assume node is restarting, so reload onopen
+    setTimeout(connect, 1000)
+  }
+
+  ws.onerror = function() {
+    console.error('Socket error! Closing socket...')
+    ws.close(); // should fire onclose and attempt reconnect
+  }
+
   ws.onmessage = function() {
     window.location.reload()
   }
 
-  ws.onclose = function() { // e.g. if node restarts
-    console.log('close')
-    setTimeout(connect, 1000)
-  }
-
-  ws.onerror = function(err) {
-    console.error('Socket error: ', err, 'Closing socket');
-    ws.close(); // should fire onclose and attempt reconnect
+  ws.onopen = function() {
+    if (isNodeRestarting) window.location.reload() // reload, which resets isNodeRestarting to false
   }
 }
 
