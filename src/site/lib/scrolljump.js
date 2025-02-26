9f5b850bc730e2ffc366101463d47e08ef787027
christ-resurrected <dev@me2christ.com>
2025-02-26 23:46:10 +0000

    refactor

--- src/site/lib/scrolljump.js
+++ src/site/lib/scrolljump.js
@@ -6,22 +6,17 @@ function getYs(h1s) {
   return h1s.map((h1) => h1.getBoundingClientRect().y)
 }
 
+function jumpTo(h1s, i) {
+  if (i < 0) return
+  h1s[i].scrollIntoView({ behavior: 'smooth' })
+}
+
 function jumpNext() {
   const h1s = getH1s()
-  const ys = getYs(h1s)
-  let i = ys.findIndex((y) => y > 0)
-  if (i < 0) return
-  h1s[i].scrollIntoView({
-    behavior: 'smooth'
-  })
+  jumpTo(h1s, getYs(h1s).findIndex((y) => y > 0))
 }
 
 function jumpPrev() {
   const h1s = getH1s().reverse()
-  const ys = getYs(h1s)
-  let i = ys.findIndex((y) => y < 0)
-  if (i < 0) return
-  h1s[i].scrollIntoView({
-    behavior: 'smooth'
-  })
+  jumpTo(h1s, getYs(h1s).findIndex((y) => y < 0))
 }
