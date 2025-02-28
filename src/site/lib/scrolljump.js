fe045ae4dda2e0328e323b1c1ee3a0bee707df3a
christ-resurrected <dev@me2christ.com>
2025-02-28 16:37:37 +0000

    refactor

--- src/site/lib/scrolljump.js
+++ src/site/lib/scrolljump.js
@@ -21,17 +21,16 @@ function jumpPrev() {
   jumpTo(h1s, getYs(h1s).findIndex((y) => y < -10))
 }
 
+// dynamically enable/disable prev/next buttons
 addEventListener("DOMContentLoaded", () => {
-  // show 'prev' if not at or above first h1
   const obsCallbackTop = ([entry]) => {
-    document.body.classList.toggle('scrolljump-prev', !entry.isIntersecting)
+    document.body.classList.toggle('scrolljump-top', entry.isIntersecting)
   }
   new IntersectionObserver(obsCallbackTop).observe(getH1s()[0])
 
-  // show 'next' if not at or below last h1
   const obsCallbackBottom = ([entry]) => {
     const isAtOrBelowLastH1 = entry.boundingClientRect.top <= 5 && !entry.isIntersecting
-    document.body.classList.toggle('scrolljump-next', !isAtOrBelowLastH1)
+    document.body.classList.toggle('scrolljump-bottom', isAtOrBelowLastH1)
   }
   const obsOpts = { rootMargin: '-5px', threshold: 1.0 }
   new IntersectionObserver(obsCallbackBottom, obsOpts).observe(getH1s().at(-1))
