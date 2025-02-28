308e5800224ef8f00d23b4bd5e7ff7d26906177d
christ-resurrected <dev@me2christ.com>
2025-02-28 16:07:50 +0000

    fix when javascript disabled

--- src/site/lib/scrolljump.js
+++ src/site/lib/scrolljump.js
@@ -22,16 +22,16 @@ function jumpPrev() {
 }
 
 addEventListener("DOMContentLoaded", () => {
-  // indicate at top of page if at or above first h1
+  // show 'prev' if not at or above first h1
   const obsCallbackTop = ([entry]) => {
-    document.body.classList.toggle('scrolljump-top', entry.isIntersecting)
+    document.body.classList.toggle('scrolljump-prev', !entry.isIntersecting)
   }
   new IntersectionObserver(obsCallbackTop).observe(getH1s()[0])
 
-  // indicate at bottom of page if at or below last h1
+  // show 'next' if not at or below last h1
   const obsCallbackBottom = ([entry]) => {
-    const isBottom = entry.boundingClientRect.top <= 5 && !entry.isIntersecting
-    document.body.classList.toggle('scrolljump-bottom', isBottom)
+    const isAtOrBelowLastH1 = entry.boundingClientRect.top <= 5 && !entry.isIntersecting
+    document.body.classList.toggle('scrolljump-next', !isAtOrBelowLastH1)
   }
   const obsOpts = { rootMargin: '-5px', threshold: 1.0 }
   new IntersectionObserver(obsCallbackBottom, obsOpts).observe(getH1s().at(-1))
