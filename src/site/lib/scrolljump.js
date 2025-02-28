2ed720ded58c2b4de6140b758cdfd98ada1ce6d9
christ-resurrected <dev@me2christ.com>
2025-02-28 15:59:32 +0000

    add scrolljump-bottom

--- src/site/lib/scrolljump.js
+++ src/site/lib/scrolljump.js
@@ -21,10 +21,18 @@ function jumpPrev() {
   jumpTo(h1s, getYs(h1s).findIndex((y) => y < -10))
 }
 
-// show/hide prev FAB depending on scroll location
 addEventListener("DOMContentLoaded", () => {
-  const obsCallback = ([entry]) => {
+  // indicate at top of page if at or above first h1
+  const obsCallbackTop = ([entry]) => {
     document.body.classList.toggle('scrolljump-top', entry.isIntersecting)
   }
-  new IntersectionObserver(obsCallback).observe(getH1s()[0])
+  new IntersectionObserver(obsCallbackTop).observe(getH1s()[0])
+
+  // indicate at bottom of page if at or below last h1
+  const obsCallbackBottom = ([entry]) => {
+    const isBottom = entry.boundingClientRect.top <= 5 && !entry.isIntersecting
+    document.body.classList.toggle('scrolljump-bottom', isBottom)
+  }
+  const obsOpts = { rootMargin: '-5px', threshold: 1.0 }
+  new IntersectionObserver(obsCallbackBottom, obsOpts).observe(getH1s().at(-1))
 })
