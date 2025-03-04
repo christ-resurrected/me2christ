3c19d87193d926fcd368424a369bd0dd7d525328
christ-resurrected <dev@me2christ.com>
2025-03-04 10:27:44 +0000

    refactor

--- src/site/lib/scrolljump.js
+++ src/site/lib/scrolljump.js
@@ -1,48 +1,18 @@
-function getLastH1() {
-  return document.querySelector('.cards:last-of-type h1')
-}
-
-function getH1s() {
-  return Array.from(document.getElementsByTagName('h1'))
-}
-
-function getYs(h1s) {
-  return h1s.map((h1) => h1.getBoundingClientRect().y)
-}
-
-function jump(h1s, i) {
-  if (i < 0) return
-  h1s[i].scrollIntoView({ behavior: 'smooth' })
-}
-
-function jumpDown() {
-  const h1s = getH1s()
-  jump(h1s, getYs(h1s).findIndex((y) => y > 10))
-}
-
-function jumpUp() {
-  const h1s = getH1s().reverse()
-  jump(h1s, getYs(h1s).findIndex((y) => y < -10))
-}
-
-function toggleJump(sel, enable) {
-  document.querySelector(sel).classList.toggle('jump-enabled', enable)
-}
-
-function toggleJumpDown(enable) {
-  toggleJump('.jump-down', enable)
-}
+function getLastH1() { return document.querySelector('.cards:last-of-type h1') }
+function getH1s() { return Array.from(document.getElementsByTagName('h1')) }
+function getYs(h1s) { return h1s.map((h1) => h1.getBoundingClientRect().y) }
+function jump(h1s, i) { if (i >= 0) h1s[i].scrollIntoView({ behavior: 'smooth' }) }
+function jumpDown() { const h1s = getH1s(); jump(h1s, getYs(h1s).findIndex((y) => y > 10)) }
+function jumpUp() { const h1s = getH1s().reverse(); jump(h1s, getYs(h1s).findIndex((y) => y < -10)) }
+function toggleJump(sel, enable) { document.querySelector(sel).classList.toggle('jump-enabled', enable) }
+function toggleJumpDown(enable) { toggleJump('.jump-down', enable) }
 
 // dynamically enable/disable jump up/down buttons
 addEventListener('DOMContentLoaded', () => {
-  const obsCallbackTop = ([entry]) => {
-    toggleJump('.jump-up', !entry.isIntersecting)
-  }
+  const obsCallbackTop = ([entry]) => { toggleJump('.jump-up', !entry.isIntersecting) }
   new IntersectionObserver(obsCallbackTop).observe(document.querySelector('h1'))
 
-  const obsCallbackBottom = ([entry]) => {
-    toggleJumpDown(entry.boundingClientRect.top > 5 || entry.isIntersecting)
-  }
+  const obsCallbackBottom = ([entry]) => { toggleJumpDown(entry.boundingClientRect.top > 5 || entry.isIntersecting) }
   new IntersectionObserver(obsCallbackBottom, { rootMargin: '-5px', threshold: 1.0 }).observe(getLastH1())
 
   // FIX/HACK: make :active pseudoclass work in iOS safari
