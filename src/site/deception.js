aa8994dd9e19fa61619a53214255704972d3f3d6
christ-resurrected <dev@me2christ.com>
2025-03-10 16:59:50 +0000

    fix scrolljump js errors

--- src/site/deception.js
+++ src/site/deception.js
@@ -1,11 +1,13 @@
 // https://stackoverflow.com/questions/7717527/smooth-scrolling-when-clicking-an-anchor-link
 addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-      e.preventDefault()
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
+    if (anchor.attributes['href'].nodeValue.length > 1) {
+      anchor.addEventListener('click', function(e) {
+        e.preventDefault()
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+        })
       })
-    })
+    }
   })
 })
